e1205b3aa4d6a3a7f40c833276cab4990fe89b31
Roach <sprdk4@rc05xcs213.managed.mst.edu>
2016-11-27 16:46:51 -0600

    modifed return of user info so that password and hash isn't included in it

--- Control/functions/getAllUsersInformation.php
+++ Control/functions/getAllUsersInformation.php
@@ -3,7 +3,7 @@ require_once "includeAllFunctions.php";
 function getUsersInformation(){
 	$con = mysqli_connect("localhost", "root", "") or die(mysql_error()); 
 	mysqli_select_db($con, "mydb") or die(mysql_error()); 
-	$results=mysqli_query("SELECT * FROM users");
+	$results=mysqli_query("SELECT username,high_score,online,email,num_games FROM users");
 	return $results;
 }
 ?>
--- Control/functions/getUsersInformation.php
+++ Control/functions/getUsersInformation.php
@@ -4,7 +4,8 @@ function getUsersInformation(){
 	$username= getUsername();
 	$con = mysqli_connect("localhost", "root", "") or die(mysql_error()); 
 	mysqli_select_db($con, "mydb") or die(mysql_error()); 
-	$results=mysqli_query("SELECT * FROM users WHERE username = '$username'");
+	//(`username`, `password`, `hash`, `high_score`, `online`, `email`, `num_games`)
+	$results=mysqli_query("SELECT username,high_score,online,email,num_games FROM users WHERE username = '$username'");
 	return $results;
 }
 ?>
