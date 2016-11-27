e0146e71d0a39f53bc890e600cd1ca5d83e169ad
beb75 <beb75@rc16xcs213.managed.mst.edu>
2016-11-27 16:37:53 -0600

    changed index to pretty format

--- Control/create_account.php
+++ Control/create_account.php
@@ -1,26 +1,38 @@
 <html>
 <body>
+<style>
+.center {
+    padding: 70px 0;
+    text-align: center;
+}
+</style>
 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<div class="center">
 <form action="strength.php" method="post">
 <td width="150" align="left" valign="top" class="FormText"> 
-<div align="left">Username</div></td>
+<div>Username</div></td>
 <td width="250"> <input name="name" type="name" id="name" class="FormText"></td>
 </tr>
 <tr>
 <td width="150" align="left" valign="top" class="FormText"> 
-<div align="left">Password</div></td>
+<div>Password</div></td>
 <td width="250"> <input name="password" type="password" id="password" class="FormText"></td>
 </tr>
 <tr>
 <td width="150" align="left" valign="top" class="FormText"> 
-<div align="left">Email Address</div></td>
+<div>Email Address</div></td>
 <td width="250"> <input name="email" type="name" id="email" class="FormText"></td>
 </tr>
 <tr>
 <br>
 <br>
-<button type="submit" name="create" value="1">Create Account</button>`
+<button class="btn btn-primary" type="submit" name="create" value="1">Create Account</button>
 </form>
+</div>
 <body>
 <html>
 
