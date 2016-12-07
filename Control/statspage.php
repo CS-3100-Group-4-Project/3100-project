a4b0abfcf810cbd43430585b9f99420845267287
beb75 <beb75@rc04xcs213.managed.mst.edu>
2016-12-06 18:02:03 -0600

    fixed

--- Control/statspage.php
+++ Control/statspage.php
@@ -3,7 +3,9 @@
 	<head>
 		<title>View Statistics</title>
 	</head>
-
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<body background="bg.png">
 	<form action="gamepage.php" method="post">
 	<button class="btn btn-primary" type="submit" name="create_acc" value="1">Return to Game</button>
